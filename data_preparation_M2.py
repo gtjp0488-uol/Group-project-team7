f96e6d68a48e56294a885dc26f1c22e80cf0be1a
xbhj123-uol <xbhj6908@leeds.ac.uk>
2026-04-20 10:57:28 +0000

    Initial Function Created

--- data_preparation_M2.py
+++ data_preparation_M2.py
@@ -1,1 +1,17 @@
+import os
+import requests
+def download_answer_files(cloud_url:str, path_to_data_folder:str, total_respondents:int):
+    os.makedirs(path_to_data_folder, exist_ok=True)
+    for i in range(1,total_respondents+1):
+         source_filename = f"a{i}.txt"
+         source_url = f"{cloud_url}/{source_filename}"
 
+         my_file = requests.get(source_url)
+         my_file.raise_for_status()
+         target_filename = f"answers_respondent_{i}.txt"
+         target_path = os.path.join(path_to_data_folder, target_filename)
+
+         with open(target_path, "w", encoding="utf-8") as f:
+             f.write(my_file.text)
+    
+download_answer_files("https://raw.githubusercontent.com/fc-leeds/MATH1604_2025_2026_data/main","data",70)
