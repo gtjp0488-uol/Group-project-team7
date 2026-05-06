2ba4126529f6b4c3f0ab4aa643f56ef362abe83c
gtjp0488-uol <gtjp0488@leeds.ac.uk>
2026-05-06 20:16:35 +0100

    Update test_M1.py

--- scripts/test_M1.py
+++ scripts/test_M1.py
@@ -2,5 +2,6 @@ from data_extraction_M1 import extract_answers_sequence
 
 # test with a sample file
 answers = extract_answers_sequence("data/a1.txt")
-
-print(answers[:10])
+# test with answers_respondent_1.txt (in moc_data)
+answers = extract_answers_sequence("moc_data/answers_respondent_1.txt")
+print(answers)
